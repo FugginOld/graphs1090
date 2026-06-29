1e3561f8e1bfd2968778676c94ae4c542b4fa874
Fuggin <121835359+FugginOld@users.noreply.github.com>
2026-06-28 22:24:19 -0400

    fix: streamline version increment command in tag script

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -3,7 +3,7 @@
 set -e
 trap 'echo "[ERROR] Error in line $LINENO when executing: $BASH_COMMAND"' ERR
 
-VERSION="1.0.$(( $(cat version | cut -d'.' -f3) + 1 ))"
+VERSION="1.0.$(( $(cut -d'.' -f3 < version) + 1 ))"
 echo "$VERSION" > version
 git add version
 
